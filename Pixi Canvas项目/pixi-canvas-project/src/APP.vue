8e7d2ef5433df6553c9679e737f5aaac86a7611e
linancy <1825326125@qq.com>
2026-03-01 19:20:21 +0800

    移动到另一仓库

--- Pixi Canvas项目/pixi-canvas-project/src/APP.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <PixiCanvas />
-</template>
-
-<script setup>
-import PixiCanvas from './components/PixiCanvas.vue'
-import './assets/fonts/iconfont1.css'
-import './assets/fonts/iconfont.css'
-</script>
